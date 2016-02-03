a74ea61e6dd66dcf01743f947f4789f7718a52cd
elkenzi <elkenziamine@gmail.com>
2016-02-03 11:50:04 +0000

    install sh

--- script/init.sh
+++
@@ -1,5 +0,0 @@
-cp -r ~/.ssh/* ./../init/ssh/
-docker-machine start default
-eval $(docker-machine env default)
-docker-compose build
-docker-compose up
---
+++ script/install.sh
@@ -0,0 +1,8 @@
+cd docker
+cp -r ~/.ssh/* ./init/ssh/
+docker-machine stop default
+docker-machine create --driver virtualbox ttp
+docker-machine start ttp
+eval $(docker-machine env ttp)
+docker-compose build
+docker-compose up

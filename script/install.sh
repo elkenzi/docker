d8fd75cf0fc682843d49ecdd1b65d7fd1b0360a8
elkenzi <elkenziamine@gmail.com>
2016-02-03 13:16:47 +0000

    default machine

--- script/install.sh
+++ script/install.sh
@@ -1,8 +1,9 @@
 cd docker
 cp -r ~/.ssh/* ./init/ssh/
-docker-machine stop default
-docker-machine create --driver virtualbox ttp
-docker-machine start ttp
-eval "$(docker-machine env ttp)"
+docker-machine start default
+#docker-machine create --driver virtualbox ttp
+#docker-machine start ttp
+#eval "$(docker-machine env ttp)"
+eval "$(docker-machine env default)" 
 docker-compose build
 docker-compose up
